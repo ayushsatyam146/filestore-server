b04fa3bd770610b450577b72a7bdd1dd3a427c66
Ayush Satyam <ayushsatyam@Ayushs-MacBook-Air.local>
2023-06-02 17:07:51 +0530

    Initializing project

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// Declare the global map variable
+var FileHash = make(map[string][]string)
+
+func main() {
+	os.Mkdir("./uploads", os.ModePerm)
+	
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Server is running on port 8080.")
+
+}
